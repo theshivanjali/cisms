9c102f8267d6e4f94bd951730fb2f418d7dd42be
the.shivanjali <shivanjali012@gmail.com>
2019-01-25 17:53:58 +0530

    To Export Data, PHP Library Added

---
+++ application/controllers/Excel_controller.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+use PhpSpreadsheet\Spreadsheet;
+use PhpSpreadsheet\Writer\Xls;
+
+class Excel_controller extends CI_Controller
+{
+		
+}
